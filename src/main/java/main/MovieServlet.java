520fb04835a42792f606753f54e3f0e940238225
Micah Wood <micahswood7@gmail.com>
2022-04-04 13:40:18 -0500

    started movie project

---
+++ src/main/java/main/MovieServlet.java
@@ -0,0 +1,10 @@
+package main;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "MovieServlet", urlPatterns = "/movies/*")
+public class MovieServlet extends HttpServlet{
+
+}
